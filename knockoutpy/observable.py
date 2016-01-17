0e0d2e2af6d9c3e6780b16911bd1c96cc0e57389
Alex <alex.kooper@gmail.com>
2016-01-17 13:58:52 -0500

    Create observable.py

---
+++ knockoutpy/observable.py
@@ -0,0 +1,29 @@
+class Observable(object):
+    def __init__(self, name=None):
+        self.name = name
+        self._subscribers = set()
+        self._dependants = set()
+    
+    def subscribe(self, f):
+        self._subscribers.add(f):
+    
+    on_change = subscribe
+    
+    def unsubscribe(self, f):
+        self._subscribers.remove(f)
+    
+    @property
+    def subscribers(self):
+        return self._subscribers
+        
+    def add_dependant(self, d):
+        self._dependants.add(d)
+    
+    def remove_dependant(self, d)
+        self._dependants.remove(d)
+        
+    @property
+    def dependants(self):
+        return self._dependants
+        
+    
