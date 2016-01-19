7fdb0ae2a4520943cfbf6b62565e18403113f68e
alex.kooper@gmail.com <alex.kooper@gmail.com>
2016-01-19 01:43:32 -0500

    First working implementation with a simple example

---
+++ examples/observable.py
@@ -0,0 +1,21 @@
+from knockoutpy.observable import InputValue, ComputedValue
+
+if __name__ == '__main__':
+    a = InputValue(5, 'a')
+
+    b = ComputedValue(lambda: a.value * 2, 'b')
+    c = ComputedValue(lambda: a.value + 3, 'c')
+    d = ComputedValue(lambda: b.value + c.value, 'd')
+
+    print('a = {}'.format(a.value))
+    print('b = {}'.format(b.value))
+    print('c = {}'.format(c.value))
+    print('d = {}'.format(d.value))
+
+    def print_value(o): 
+        print('{} value changed to {}'.format(o.name, o.value))
+
+    d.on_change(print_value)
+
+    a = 3
+
